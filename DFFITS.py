af1c2f330b9a123d097724a39cf2565542bd37b3
aerioeus <aerioeus@MBPM2.local>
2024-04-25 09:39:31 +0200

    -multiple regression added

--- DFFITS.py
+++ DFFITS.py
@@ -1,14 +1,22 @@
+"""
+This script performs a linear regression analysis using the OLS model from statsmodels.
+It adds a constant to the data for the intercept, fits the model, and calculates
+various statistics such as predicted values, residuals, and DFFITS values.
+"""
+
 import numpy as np
 import pandas as pd
 from statsmodels.api import OLS, add_constant
 
+
+
 # Datenbasis
 data = {
-    "X": [0.10000, 0.45401, 1.09765, 1.27936, 2.20611, 2.50064, 3.04030, 3.23583, 4.45308, 
-          4.16990, 5.28474, 5.59238, 5.92091, 6.66066, 6.79953, 7.97943, 8.41536, 8.71607, 
+    "X": [0.10000, 0.45401, 1.09765, 1.27936, 2.20611, 2.50064, 3.04030, 3.23583, 4.45308,
+          4.16990, 5.28474, 5.59238, 5.92091, 6.66066, 6.79953, 7.97943, 8.41536, 8.71607,
           8.70156, 9.16463, 4.00000],
-    "y": [-0.0716, 4.1673, 6.5703, 13.8150, 11.4501, 12.9554, 20.1575, 17.5633, 26.0317, 
-          22.7573, 26.3030, 30.6885, 33.9402, 30.9228, 34.1100, 44.4536, 46.5022, 50.0568, 
+    "y": [-0.0716, 4.1673, 6.5703, 13.8150, 11.4501, 12.9554, 20.1575, 17.5633, 26.0317,
+          22.7573, 26.3030, 30.6885, 33.9402, 30.9228, 34.1100, 44.4536, 46.5022, 50.0568,
           46.5475, 45.7762, 40.0000]
 }
 
@@ -40,6 +48,4 @@ results = pd.DataFrame({
     "residuals": df['residuals'],
     "DFFITS": dffits
 })
-
-results
 print(results)
